5538ce4525d87ff65bd06669f841f281fff4cba3
Hugo <hugolacerdar@gmail.com>
2021-09-20 23:41:23 -0300

    Add conditional mapping for transfer operations

--- src/modules/statements/mappers/BalanceMap.ts
+++ src/modules/statements/mappers/BalanceMap.ts
@@ -1,28 +1,64 @@
 import { Statement } from "../entities/Statement";
 
 export class BalanceMap {
-  static toDTO({statement, balance}: { statement: Statement[], balance: number}) {
-    const parsedStatement = statement.map(({
-      id,
-      amount,
-      description,
-      type,
-      created_at,
-      updated_at
-    }) => (
-      {
+  static toDTO({
+    statement,
+    balance,
+  }: {
+    statement: Statement[];
+    balance: number;
+  }) {
+    const parsedStatement = statement.map(
+      ({
         id,
-        amount: Number(amount),
+        user_id,
+        sender_id,
+        receiver_id,
+        amount,
         description,
         type,
         created_at,
-        updated_at
+        updated_at,
+      }) => {
+        if (type === "transfer" && (sender_id || user_id === receiver_id)) {
+          return {
+            id,
+            user_id,
+            sender_id,
+            amount: Number(amount),
+            description,
+            type,
+            created_at,
+            updated_at,
+          };
+        }
+        if (type === "transfer" && receiver_id) {
+          return {
+            id,
+            user_id,
+            receiver_id,
+            amount: Number(amount),
+            description,
+            type,
+            created_at,
+            updated_at,
+          };
+        }
+        return {
+          id,
+          user_id,
+          amount: Number(amount),
+          description,
+          type,
+          created_at,
+          updated_at,
+        };
       }
-    ));
+    );
 
     return {
       statement: parsedStatement,
-      balance: Number(balance)
-    }
+      balance: Number(balance),
+    };
   }
 }
