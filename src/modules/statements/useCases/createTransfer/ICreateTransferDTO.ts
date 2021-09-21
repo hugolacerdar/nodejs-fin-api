de2689161f2c397f99820e24d8464124be4c9da4
Hugo <hugolacerdar@gmail.com>
2021-09-20 23:45:02 -0300

    Add Create Transfer DTO

---
+++ src/modules/statements/useCases/createTransfer/ICreateTransferDTO.ts
@@ -0,0 +1,8 @@
+import { Statement } from "../../entities/Statement";
+
+export interface ICreateTransferDTO {
+  sender_id: string;
+  receiver_id: string;
+  amount: number;
+  description: string;
+}
